8725809375aade9c16c39c2b4fe336c7d744dbac
Lukáš Frantál <lukas.frantal@techfides.cz>
2019-02-09 18:51:19 +0100

    registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app, used Angular Dependency Injection to inject it into a component, gave the HeroService get data method an asynchronous signature, discovered Observable and the RxJS Observable library, used RxJS of() to return an observable of mock heroes (Observable<Hero[]>), component's ngOnInit lifecycle hook calls the HeroService method, not the constructor,

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  constructor() {}
+  // public getHeroes(): Array<Hero> {
+  //   return HEROES;
+  // }
 
-  constructor() { }
+  public getHeroes(): Observable<Array<Hero>> {
+    return of(HEROES);
+  }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +8,25 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  public heroes: Array<Hero> = HEROES;
+  public heroes: Array<Hero>;
   public selectedHero: Hero;
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
   public onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  // public getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  public getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .subscribe((heroes: Array<Hero>) => (this.heroes = heroes));
+  }
 }
